4e4e82f8315c8c8450f920daefa1f05bb33887f0
Vikash Harjeewan <vikash@backgroundcheck.email>
2018-04-28 17:11:23 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -1,17 +1,33 @@
 <?
 // This is a template for a PHP scraper on morph.io (https://morph.io)
 // including some code snippets below that you should find helpful
+//  Roll No Start   - >	300001 
+//  Roll No End	    - >	300001 
+//  PDF Link	    - > https://www.fbise.edu.pk/notifications/hssc/HSSC-I-A%202017%20Gazette.pdf
+/*
+Created by 		Vikash 
+Date of Creation 	4/28/2018
+Email 			vikashbaria4@gmail.com
+skype			vikashharjeewan2@Hotmail.com
+Expert in web Scraping,Web automation,Investigation research and databases,
+*/
+
+
 
 require 'scraperwiki.php';
 require 'scraperwiki/simple_html_dom.php';
 
-for($page = 300842;$page <= 300843; $page++)
+
+
+
+for($page = 300001;$page <= 397031; $page++)
 	{
 		$link	=	'https://www.fbise.edu.pk/linkrollno-hssc-1.php?roll_no='.$page;
+		echo 	"$page\n";
 		$browser	=	file_get_html($link);
 		if($browser)
 		{
-			$nameofresult	=	$browser->find("//*[@id='item']/table[1]/tbody/tr/td/font/b",0)->plaintext;
+			$nameofresult		=	$browser->find("//*[@id='item']/table[1]/tbody/tr/td/font/b",0)->plaintext;
 			$rollno 		=	$browser->find("//*[@id='item']/table[2]/tbody/tr[1]/td[2]",0)->plaintext;
 			$name 			=	$browser->find("//*[@id='item']/table[2]/tbody/tr[3]/td[2]",0)->plaintext;
 			$fname			=	$browser->find("//*[@id='item']/table[2]/tbody/tr[4]/td[2]",0)->plaintext;
